35d43027b82213d1c040508669c79467e668065c
cc1005 <conorkampbell@hotmail.com>
2021-05-02 22:18:11 +0100

    Created a cached reference to the Audio Source and made sure it only played when pushing thrust button using if/else statement.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,14 @@ public class Movement : MonoBehaviour
     [SerializeField] float thrustForceY = 1000f;
     [SerializeField] float rotateSpeed = 100f;
     Rigidbody myRigidbody;
+    AudioSource myAudioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,10 +26,18 @@ public class Movement : MonoBehaviour
 
     void ProcessThrust()
     {
-       if (Input.GetKey(KeyCode.Space))
-       {
-           myRigidbody.AddRelativeForce(Vector3.up * thrustForceY * Time.deltaTime);
-       }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidbody.AddRelativeForce(Vector3.up * thrustForceY * Time.deltaTime);
+            if(!myAudioSource.isPlaying)
+            {
+                myAudioSource.Play();
+            }
+        }
+        else
+        {
+            myAudioSource.Stop();
+        }
     }
     
     void ProcessRotation()
